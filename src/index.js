2fdd328632e70bd9169a732625ade1764b6e7c00
Monica <monicalagdaan@hotmail.com>
2023-07-30 10:30:07 -0700

    set up firebase configuration files, testing firebase interactionwith book data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +18,40 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBY0kHWNvFvSwi-TW_vy3A9NXTpCOkcBeY",
+  authDomain: "sparkle-front-end.firebaseapp.com",
+  projectId: "sparkle-front-end",
+  storageBucket: "sparkle-front-end.appspot.com",
+  messagingSenderId: "936939480448",
+  appId: "1:936939480448:web:62ce7f49bf3987105d0d65"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+.then ((snapshot) => {
+  let books = []
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(books)
+})
+.catch(err => {
+  console.log(err.message)
+})
